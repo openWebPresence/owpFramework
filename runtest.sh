3783c59ae8f2fb202bf1feef0403ad88e31b870b
Brian Tafoya <btafoya@briantafoya.com>
2017-02-27 10:25:48 -0500

    Add some more cleanup

--- runtest.sh
+++ runtest.sh
@@ -6,5 +6,6 @@ composer -n -q update
 /usr/bin/mysql owp_users < ./schema/owp-users-complete.sql
 ./vendor/bin/phpunit --verbose --colors=never > ./logs/phpunit.log
 ./vendor/bin/phpdoc -c ./phpdoc.tpl.xml > ./logs/phpdoc.log
+rm -rf ./docs/phpdoc-cache-*
 ./vendor/bin/phpcbf -n --tab-width=4 ./src/*.php >> ./logs/phpcs.log
 ./vendor/bin/phpcbf -n --tab-width=4 ./tests/*.php >> ./logs/phpcs.log
